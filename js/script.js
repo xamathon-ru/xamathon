f71f01872f788d71011d0c95275b123ee016a319
walkinded <labunskij1997@gmail.com>
2021-04-05 22:26:19 +0300

    xamathon

---
+++ js/script.js
@@ -0,0 +1,20 @@
+ function textNull(){ // Функция возвращает текст в исходный вид
+    document.getElementById('text').innerHTML = '';
+}
+
+function text1(){ // Функция изменяет текст
+    document.getElementById('text').innerHTML = 'Непрерывная менторская поддержка и обратная связь от экспертов хакатона';
+ }
+
+function text2(){ // Функция изменяет текст
+    document.getElementById('text').innerHTML = 'Возможность создать команду, познакомиться с единомышленниками';
+}
+
+ function text3(){ // Функция изменяет текст
+   document.getElementById('text').innerHTML = 'Денежный приз, сертификаты, всеобщее признание';
+ }
+
+ function text4(){ // Функция изменяет текст
+   
+    document.getElementById('text').innerHTML = 'Прими участие в разработке реального продукта';
+ }
